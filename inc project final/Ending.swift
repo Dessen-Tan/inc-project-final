20c64499b1ac373390059a524e700317f033b673
Dessen-Tan <dessen_arwin_tan_ahn@s2024.ssts.edu.sg>
2024-10-29 23:08:24 +0800

    Update Ending.swift

--- inc project final/Ending.swift
+++ inc project final/Ending.swift
@@ -5,14 +5,25 @@
 //  Created by Dessen Tan on 20/10/24.
 //
 
+
 import SwiftUI
+import AVKit
+import AVFoundation
 
 struct Ending: View {
+    @State private var player = AVPlayer()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Rectangle()
+                .foregroundStyle(.black)
+                .frame(width: 1000, height: 1000)
+            VStack {
+                VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "SST inc ending", withExtension: "mp4")!))
+                    .frame(width: 500, height: 900)
+            }
+        }
     }
 }
-
 #Preview {
     Ending()
 }
